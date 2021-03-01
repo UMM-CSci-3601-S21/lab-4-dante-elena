51499214c73184c0f4ed88e2f31488eeb81b71d5
Dante <Mill8622@morris.umn.edu>
2021-03-01 15:16:06 -0700

    Delete Test Bed Duplicate
    Deleted a test bed duplicate
    
    Co-authored By: Elena Lam<lam00071@morris.umn.edu>

--- client/src/app/todos/add-todo.component.spec.ts
+++ client/src/app/todos/add-todo.component.spec.ts
@@ -2,8 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule, FormGroup, AbstractControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { Todo } from './todo';
+import { Todo} from './todo';
 import { TodoService } from './todo.service';
 
 describe('Todo service: ', () => {
